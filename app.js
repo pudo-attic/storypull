edc17d4222eb09f2a641c95cb9a886ebd405c295
Friedrich Lindenberg <friedrich@pudo.org>
2013-10-17 19:39:29 +0100

    Begin implementing the graf editing API.

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ function makeUri(req, path) {
     return req.protocol + "://" + req.get('host') + path;
 }
 
+function makeUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+}
+
 var app = express();
 app.use(express.logger());
 app.use(express.errorHandler());
@@ -87,10 +94,15 @@ app.post('/api/import', function(req, res){
 
         stories.forEach(function(story) {
             var grafs = story.grafs || [];
+            story.created_at = new Date();
+            story.last_change = story.created_at;
 
             grafsColl.remove({'story': story.slug}, {}, function(err, docs) {
-                grafs.forEach(function(graf) {
+                grafs.forEach(function(graf, sequence) {
                     graf.story = story.slug;
+                    graf.sequence = sequence + 1;
+                    graf.key = makeUUID();
+                    graf.created_at = story.created_at;
                     grafsColl.insert(graf, function(err, docs) {});
                 });
             });
@@ -123,7 +135,7 @@ app.get('/api/stories/:slug', function(req, res) {
             if (story===null) {
                 res.status(404).jsonp({'status': 'Not found'});
             }
-            grafsColl.find({'story': story.slug}).toArray(function(err, results) {
+            grafsColl.find({'story': story.slug}, {'sort': [['sequence', 1], ['created_at', 1]]}).toArray(function(err, results) {
                 story.grafs = results;
                 res.jsonp(story);
             });
@@ -132,7 +144,45 @@ app.get('/api/stories/:slug', function(req, res) {
     });
 });
 
+app.post('/api/stories/:slug/graf', function(req, res) {
+    dbPromise.done(function(db) {
+        var storiesColl = db.collection('stories');
+        var grafsColl = db.collection('grafs');
+        var graf = req.body;
+
+        if (!req.user) {
+            res.status(401).jsonp({'status': 'Not logged in'});
+        }
+
+        if (!graf || !graf.text || graf.text.length===0) {
+            res.status(400).jsonp({'status': 'No text'});
+        }
+
+        storiesColl.findOne({'slug': req.params.slug}, function(err, story) {
+            if (!story) {
+                res.status(404).jsonp({'status': 'No such story'});
+            }
+            graf.latest = true;
+            graf.author = req.user.screen_name;
+            graf.approved = (story.author === req.user.screen_name);
+            graf.created_at = new Date();
+            graf.key = graf.key || makeUUID();
+
+            // todo: generate and update sequence
+
+            storiesColl.update({'slug': story.slug}, {'$set': {'last_change': graf.created_at}},
+                {}, function(err, docs) {});
+            var query = graf.approved ? {'$set': {'latest': false}} : {'$set': {'current': false, 'latest': false}};
+            grafsColl.update({'slug': story.slug, 'key': graf.key}, query, {}, function(err, docs) {
+                grafsColl.insert(graf, function(err, docs) {
+                    res.jsonp(docs);
+                });
+            });
+        });
+    });
+});
+
 var port = parseInt(process.env.PORT, 10) || 3000;
 app.listen(port);
-console.log('Listening on port ' + port);
+//console.log('Listening on port ' + port);
 
