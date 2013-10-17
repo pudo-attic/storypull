ef29eed1908a0d4c8aa8ab6156331710a972290e
Friedrich Lindenberg <friedrich@pudo.org>
2013-10-17 19:18:30 +0200

    Implement twitter login.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express'),
     mongo = require('mongodb'),
-    Q = require('q');
+    Q = require('q'),
+    passport = require('passport'),
+    TwitterStrategy = require('passport-twitter').Strategy;
+
 
 var mongoUri = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL ||
@@ -19,15 +22,61 @@ function makeUri(req, path) {
 
 var app = express();
 app.use(express.logger());
-app.use(express.bodyParser());
 app.use(express.errorHandler());
 app.use(express.static(__dirname + '/static'));
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.session({ secret: process.env.SECRET || 'foobar' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+
+passport.use(new TwitterStrategy({
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: process.env.TWITTER_CALLBACK_URL
+  },
+  function(token, tokenSecret, profile, done) {
+    dbPromise.done(function(db) {
+        var usersColl = db.collection('users');
+        delete profile._json.status;
+
+        usersColl.findOne({'screen_name': profile.username}, function(err, user) {
+            if (user!==null) {
+                done(null, user);
+            }
+            usersColl.insert(profile._json, function(err, docs) {
+                done(null, docs);
+            });
+        });
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.screen_name);
+});
+
+passport.deserializeUser(function(id, done) {
+    dbPromise.done(function(db) {
+        var usersColl = db.collection('users');
+        usersColl.findOne({'screen_name': id}, function(err, user) {
+            done(err, user);
+        });
+    });
+});
 
 app.get('/api/status', function(req, res) {
     dbPromise.done(function(db) {
-        res.jsonp({'status': 'ok'});
+        res.jsonp({'status': 'ok', 'user': req.user});
     });
 });
+
+app.get('/api/auth/twitter', passport.authenticate('twitter'));
+
+app.get('/api/auth/callback',
+  passport.authenticate('twitter', { successRedirect: '/',
+                                     failureRedirect: '/' }));
 
 app.post('/api/import', function(req, res){
     dbPromise.done(function(db) {
@@ -69,6 +118,7 @@ app.get('/api/stories/:slug', function(req, res) {
         var grafsColl = db.collection('grafs');
 
         storiesColl.findOne({'slug': req.params.slug}, function(err, story) {
+            story.uri = makeUri(req, '/api/stories/' + story.slug);
             if (story===null) {
                 res.status(404).jsonp({'status': 'Not found'});
             }
