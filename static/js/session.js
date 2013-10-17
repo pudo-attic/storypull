f6627edc76f7857c5aa21a06545eb073ef4ba681
Friedrich Lindenberg <friedrich@pudo.org>
2013-10-17 21:50:38 +0100

    wrap in unload.

--- static/js/session.js
+++ static/js/session.js
@@ -1,8 +1,10 @@
-$.getJSON( "/api/status", function(data) {
-    var loggedIn = !!data.user;
-    if(loggedIn) {
-        $('#current-user').html(data.user.name || data.user.screen_name);
-    }
-    $('#sign-in').toggle(!loggedIn);
-    $('#session-status').toggle(loggedIn);
+$(function() {
+    $.getJSON( "/api/status", function(data) {
+        var loggedIn = !!data.user;
+        if(loggedIn) {
+            $('#current-user').html(data.user.name || data.user.screen_name);
+        }
+        $('#sign-in').toggle(!loggedIn);
+        $('#session-status').toggle(loggedIn);
+    });
 });
