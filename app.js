90338643068d4d0f9c9675fd5da7659440cc2142
Friedrich Lindenberg <friedrich@pudo.org>
2013-10-17 17:09:41 +0200

    put db connection in a promise

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var mongo = require('mongodb');
+var express = require('express'),
+    mongo = require('mongodb'),
+    Q = require('q');
 
 var mongoUri = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL ||
   'mongodb://localhost/newspull';
 
+var dbQ = Q.defer(),
+    dbPromise = dbQ.promise;
+
 mongo.Db.connect(mongoUri, function (err, db) {
-    console.log('success!');
+    dbQ.resolve(db);
 });
 
 var app = express();
@@ -14,8 +18,11 @@ app.use(express.logger());
 app.use(express.errorHandler());
 app.use(express.static(__dirname + '/static'));
 
-app.get('/', function(req, res){
-    res.send('Hello World');
+app.get('/', function(req, res) {
+    dbPromise.done(function(db) {
+        console.log(db);
+        res.send('Hello World');
+    });
 });
 
 var port = parseInt(process.env.PORT, 10) || 3000;
